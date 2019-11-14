680409424b334f11deb79702fc38fd66909413f3
Oxy <anoschenko.ov@phystech.edu>
2019-11-14 16:13:57 +0300

    Дз №5

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    is_group_chat = models.BooleanField(default=False)
+    topic = models.TextField()
+    last_message = models.TextField()
+
+class Message(models.Model):
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    content = models.TextField()
+    added_at = models.DateTimeField()
+
+class Attachment(models.Model):
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
+    message = models.ForeignKey('Message', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    type = models.CharField(max_length=16)
+    url = models.URLField()
